f588c71d99715bf1ced1085e59308287d96b5fde
Nigel Schuster <nigel.schusters@googlemail.com>
2017-09-18 20:23:28 -0400

    Relay <3

--- authentication/graphene.py
+++ authentication/graphene.py
@@ -33,6 +33,7 @@ class Query(AbstractType):
         """
         The viewer represents the current logged in user
         """
+        print(context.user)
         if not context.user.is_authenticated():
             return None
         return context.user
---
+++ web/js/relay/network.js
@@ -0,0 +1,24 @@
+const { Network } = require('relay-runtime');
+
+// Define a function that fetches the results of an operation (query/mutation/etc)
+// and returns its results as a Promise:
+function fetchQuery(
+  operation,
+  variables,
+) {
+  return fetch('/graphql', {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      // Add authentication and other headers here
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: operation.text, // GraphQL text from input
+      variables,
+    }),
+  }).then(response => response.json());
+}
+
+// Create a network layer from the fetch function
+export default Network.create(fetchQuery);
