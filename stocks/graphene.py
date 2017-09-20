c9929074e6f8efd0a4abfd65466c149b27e3469e
Nigel Schuster <nigel.schusters@googlemail.com>
2017-09-20 09:29:31 -0400

    Expose adding stock via GraphQL,  Query past data on Stock save

--- stocks/graphene.py
+++ stocks/graphene.py
@@ -3,6 +3,7 @@ GraphQL definitions for the Stocks App
 """
 from graphene_django import DjangoObjectType
 from graphene import AbstractType, relay
+import graphene
 from .models import DailyStockQuote, Stock
 
 
@@ -29,6 +30,31 @@ class GDailyStockQuote(DjangoObjectType):
         """
         model = DailyStockQuote
         interfaces = (relay.Node, )
+
+
+class AddStock(graphene.Mutation):
+    """
+    AddStock creates a new Stock that is tracked
+    """
+    class Input:
+        """
+        Input to create a stock. We only need the ticker.
+        """
+        ticker = graphene.NonNull(graphene.String)
+        name = graphene.NonNull(graphene.String)
+    stock = graphene.Field(lambda: GStock)
+
+    @staticmethod
+    def mutate(_, args, __, ___):
+        """
+        Creates a Stock and saves it to the DB
+        """
+        stock = Stock(
+            ticker=args['ticker'],
+            name=args['name']
+        )
+        stock.save()
+        return AddStock(stock=stock)
 
 
 # pylint: disable=no-init
--- stocks/historical.py
+++ stocks/historical.py
@@ -1,8 +1,28 @@
 """This module is for loading historical data for stocks"""
 from yahoo_historical import Fetcher
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 import arrow
 from .models import Stock, DailyStockQuote
+
+
+def fill_quote_history(stock):
+    """
+    Given a stock, it fills in the last 10 years of historical data
+    """
+    now = arrow.now()
+    ten_back = now.replace(years=-10)
+    now = get_date_array_for_fetcher(now)
+    ten_back = get_date_array_for_fetcher(ten_back)
+    fetcher = Fetcher(stock.ticker, ten_back, now)
+    history = fetcher.getHistorical()
+    objects = []
+    for row in history.itertuples():
+        value = getattr(row, "Close")
+        date = getattr(row, "Date")
+        objects.append(
+            DailyStockQuote(value=value, date=date, stock=stock))
+    DailyStockQuote.objects.bulk_create(objects, batch_size=500)
 
 
 def data_ten_years_back_for_stock(request):
@@ -15,21 +35,17 @@ def data_ten_years_back_for_stock(request):
         ticker = body['ticker']
         stock = Stock(name=name, ticker=ticker)
         stock.save()
-        now = arrow.now()
-        ten_back = now.replace(years=-10)
-        now = get_date_array_for_fetcher(now)
-        ten_back = get_date_array_for_fetcher(ten_back)
-        fetcher = Fetcher(ticker, ten_back, now)
-        history = fetcher.getHistorical()
-        objects = []
-        for row in history.itertuples():
-            value = getattr(row, "Close")
-            date = getattr(row, "Date")
-            objects.append(
-                DailyStockQuote(value=value, date=date, stock=stock))
-        DailyStockQuote.objects.bulk_create(objects, batch_size=500)
 
 
 def get_date_array_for_fetcher(arrow_date):
     """Function that formats arrow date to Yahoo Fetcher format """
     return [arrow_date.year, arrow_date.month, arrow_date.day]
+
+
+@receiver(post_save, sender=Stock)
+def create_stock(instance, created, **_):
+    """
+    Queries stock quotes when stock is created
+    """
+    if created:
+        fill_quote_history(instance)
