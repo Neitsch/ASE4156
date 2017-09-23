07495486af8575cb50c983a0215a0b80e589c826
Nigel Schuster <nigel.schusters@googlemail.com>
2017-09-23 10:47:59 -0400

    Ran lazyspec and added ci flag

---
+++ web/js/components/__tests__/shares-test.js
@@ -0,0 +1,9 @@
+/* @lazyspec (remove to manage manually) */
+/* eslint-disable */
+import shares from '../shares.jsx';
+
+describe('shares', () => {
+  it('exists', () => {
+    expect(shares).toBeTruthy();
+  });
+});
