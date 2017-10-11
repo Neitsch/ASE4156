945d031fd45caa87c32f93c674f077929df6bd21
Nigel Schuster <nigel.schusters@googlemail.com>
2017-10-11 13:13:57 -0400

    Fix TC

--- web/js/components/PersonalStatus/__tests__/PersonalStatus-test.js
+++ web/js/components/PersonalStatus/__tests__/PersonalStatus-test.js
@@ -2,11 +2,6 @@
 /* eslint-disable */
 import PersonalStatus from '../PersonalStatus.jsx';
 
-import React from 'react';
-import { shallow } from 'enzyme';
-import { shallowToJson } from 'enzyme-to-json';
-import fakeProps from 'react-fake-props';
-
 describe('PersonalStatus', () => {
   it('exists', () => {
     expect(PersonalStatus).toBeTruthy();
