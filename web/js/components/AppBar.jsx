f0373cfa0e094bc7f03d78e9cc8813f129c9bd12
jordan vega <jomivega400@gmail.com>
2017-10-15 19:51:23 -0400

    Eslint fix in AppBar

--- web/js/components/AppBar.jsx
+++ web/js/components/AppBar.jsx
@@ -55,7 +55,7 @@ function ButtonAppBar(props) {
 }
 
 ButtonAppBar.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
 };
 
 export default withStyles(styles)(ButtonAppBar);
