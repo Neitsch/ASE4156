45545fc0ffe6d1f1e4314e0c7209e9367289a556
christophe <infobiac1@gmail.com>
2017-10-30 17:23:13 -0400

    flake8 is dum

--- trading/views.py
+++ trading/views.py
@@ -1,16 +1,16 @@
 """
 Views for trading
 """
-from django.shortcuts import render
 from django.http import HttpResponse
-from stocks.models import InvestmentBucket, InvestmentBucketDescription, InvestmentStockConfiguration
+from stocks.models import InvestmentBucket
+
 
 def test(request):
     """
     Dev method for buckets
     """
     i = InvestmentBucket.objects.all()
-    t=[]
+    t = []
     for x in i:
         t.append(x.get_stock_configs())
     return HttpResponse(t)
