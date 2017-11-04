1170a671a24f26c63d84a74bac1e9e8d29991be0
Nigel Schuster <nigel.schusters@googlemail.com>
2017-11-04 14:45:02 -0400

    Finish stock black box tests

--- tests/test_stocks_model.py
+++ tests/test_stocks_model.py
@@ -8,6 +8,8 @@ from stocks.historical import create_stock
 from stocks.models import Stock
 from django.db.models.signals import post_save
 from yahoo_historical import Fetcher
+from django.contrib.auth.models import User
+from trading.models import TradeStock
 
 
 def setup_module(module):
@@ -79,3 +81,69 @@ def test_stock_find_stock():
     TestCase.assertCountEqual(None, [stock1, stock2], Stock.find_stock("Name"))
     TestCase.assertCountEqual(None, [stock1], Stock.find_stock("Name1"))
     TestCase.assertCountEqual(None, [stock2], Stock.find_stock("e2"))
+
+@pytest.mark.django_db(transaction=True)
+def test_create_new_stock():
+    """
+    Tests Stock.create_new_stock()
+    """
+    Stock.create_new_stock(ticker="ABC", name="DEF")
+    with mock.patch.object(Fetcher, "__init__", side_effect=KeyError()):
+        with pytest.raises(Exception):
+            Stock.create_new_stock(ticker="ABC", name="DEF")
+
+@pytest.mark.django_db(transaction=True)
+def test_quote_in_range():
+    """
+    Tests Stock.quote_in_range()
+    """
+    stock = Stock(
+        name="Name1X",
+        ticker="TKRC"
+    )
+    stock.save()
+    q1 = stock.daily_quote.create(
+        value=3,
+        date="2016-06-03"
+    )
+    q3 = stock.daily_quote.create(
+        value=5,
+        date="2016-06-06"
+    )
+    q2 = stock.daily_quote.create(
+        value=4,
+        date="2016-06-05"
+    )
+    assert [q1, q2, q3] == list(stock.quote_in_range())
+    assert [q1, q2, q3] == list(stock.quote_in_range(start="2016-06-03", end="2016-06-06"))
+    assert [] == list(stock.quote_in_range(start="2016-06-03", end="2016-06-02"))
+    assert [q1, q2, q3] == list(stock.quote_in_range(start="2016-06-03"))
+    assert [q2, q3] == list(stock.quote_in_range(start="2016-06-04"))
+    assert [q1, q2, q3] == list(stock.quote_in_range(end="2016-06-06"))
+    assert [q1, q2] == list(stock.quote_in_range(end="2016-06-05"))
+
+@pytest.mark.django_db(transaction=True)
+def test_trades_for_profile():
+    """
+    Tests Stock.trades_for_profile()
+    """
+    u1 = User.objects.create(username='user1', password="a")
+    u2 = User.objects.create(username='user2', password="a")
+    t1_1 = u1.profile.trading_accounts.create(
+        account_name="u1t1"
+    )
+    t1_2 = u1.profile.trading_accounts.create(
+        account_name="u1t2"
+    )
+    t2_1 = u2.profile.trading_accounts.create(
+        account_name="u1t"
+    )
+    stock = Stock.create_new_stock(
+        name="Name1X",
+        ticker="TKRC"
+    )
+    TradeStock(quantity=1, account=t1_1, stock=stock).save()
+    TradeStock(quantity=1, account=t1_2, stock=stock).save()
+    TradeStock(quantity=1, account=t2_1, stock=stock).save()
+    assert stock.trades_for_profile(u1.profile).count() == 2
+    assert stock.trades_for_profile(u2.profile).count() == 1
