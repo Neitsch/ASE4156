81ba1da0bdc46036e04ef42d4419227de419661e
Nigel Schuster <nigel.schusters@googlemail.com>
2017-11-05 09:52:48 -0500

    Fix more lint

--- tests/test_stocks_model.py
+++ tests/test_stocks_model.py
@@ -7,8 +7,8 @@ import pytest
 from stocks.historical import create_stock
 from stocks.models import InvestmentBucket, Stock
 from django.db.models.signals import post_save
-from yahoo_historical import Fetcher
 from django.contrib.auth.models import User
+from yahoo_historical import Fetcher
 from trading.models import TradeStock
 
 
@@ -104,25 +104,29 @@ def test_stock_quote_in_range():
         ticker="TKRC"
     )
     stock.save()
-    q1 = stock.daily_quote.create(
+    quote1 = stock.daily_quote.create(
         value=3,
         date="2016-06-03"
     )
-    q3 = stock.daily_quote.create(
+    quote3 = stock.daily_quote.create(
         value=5,
         date="2016-06-06"
     )
-    q2 = stock.daily_quote.create(
+    quote2 = stock.daily_quote.create(
         value=4,
         date="2016-06-05"
     )
-    assert [q1, q2, q3] == list(stock.quote_in_range())
-    assert [q1, q2, q3] == list(stock.quote_in_range(start="2016-06-03", end="2016-06-06"))
+    assert [quote1, quote2, quote3] == list(stock.quote_in_range())
+    assert [
+        quote1,
+        quote2,
+        quote3
+    ] == list(stock.quote_in_range(start="2016-06-03", end="2016-06-06"))
     assert [] == list(stock.quote_in_range(start="2016-06-03", end="2016-06-02"))
-    assert [q1, q2, q3] == list(stock.quote_in_range(start="2016-06-03"))
-    assert [q2, q3] == list(stock.quote_in_range(start="2016-06-04"))
-    assert [q1, q2, q3] == list(stock.quote_in_range(end="2016-06-06"))
-    assert [q1, q2] == list(stock.quote_in_range(end="2016-06-05"))
+    assert [quote1, quote2, quote3] == list(stock.quote_in_range(start="2016-06-03"))
+    assert [quote2, quote3] == list(stock.quote_in_range(start="2016-06-04"))
+    assert [quote1, quote2, quote3] == list(stock.quote_in_range(end="2016-06-06"))
+    assert [quote1, quote2] == list(stock.quote_in_range(end="2016-06-05"))
 
 
 @pytest.mark.django_db(transaction=True)
@@ -130,16 +134,16 @@ def test_stock_trades_for_profile():
     """
     Tests Stock.trades_for_profile()
     """
-    u1 = User.objects.create(username='user1', password="a")
-    u2 = User.objects.create(username='user2', password="a")
-    t1_1 = u1.profile.trading_accounts.create(
+    user1 = User.objects.create(username='user1', password="a")
+    user2 = User.objects.create(username='user2', password="a")
+    t1_1 = user1.profile.trading_accounts.create(
         account_name="u1t1"
     )
-    t1_2 = u1.profile.trading_accounts.create(
+    t1_2 = user1.profile.trading_accounts.create(
         account_name="u1t2"
     )
-    t2_1 = u2.profile.trading_accounts.create(
-        account_name="u1t"
+    t2_1 = user2.profile.trading_accounts.create(
+        account_name="u2t"
     )
     stock = Stock.create_new_stock(
         name="Name1X",
@@ -148,17 +152,20 @@ def test_stock_trades_for_profile():
     TradeStock(quantity=1, account=t1_1, stock=stock).save()
     TradeStock(quantity=1, account=t1_2, stock=stock).save()
     TradeStock(quantity=1, account=t2_1, stock=stock).save()
-    assert stock.trades_for_profile(u1.profile).count() == 2
-    assert stock.trades_for_profile(u2.profile).count() == 1
+    assert stock.trades_for_profile(user1.profile).count() == 2
+    assert stock.trades_for_profile(user2.profile).count() == 1
 
 
 @pytest.mark.django_db(transaction=True)
-def test_investmentbucket_trades_for_profile():
-    u1 = User.objects.create(username='user1', password="a")
-    u2 = User.objects.create(username='user2', password="a")
-    InvestmentBucket(name="B1", owner=u1.profile, public=False, available=1).save()
-    InvestmentBucket(name="B2", owner=u1.profile, public=True, available=1).save()
-    InvestmentBucket(name="B3", owner=u1.profile, public=False, available=1).save()
-    InvestmentBucket(name="B4", owner=u2.profile, public=False, available=1).save()
-    InvestmentBucket.accessible_buckets(u1.profile).count() == 3
-    InvestmentBucket.accessible_buckets(u2.profile).count() == 2
+def test_bucket_trades_for_profile():
+    """
+    Tests InvestmentBucket.trades_for_profile()
+    """
+    user1 = User.objects.create(username='user1', password="a")
+    user2 = User.objects.create(username='user2', password="a")
+    InvestmentBucket(name="B1", owner=user1.profile, public=False, available=1).save()
+    InvestmentBucket(name="B2", owner=user1.profile, public=True, available=1).save()
+    InvestmentBucket(name="B3", owner=user1.profile, public=False, available=1).save()
+    InvestmentBucket(name="B4", owner=user2.profile, public=False, available=1).save()
+    assert InvestmentBucket.accessible_buckets(user1.profile).count() == 3
+    assert InvestmentBucket.accessible_buckets(user2.profile).count() == 2
