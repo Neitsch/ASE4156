d0c6dfed17120d6d2e63c4446d88a61c6468dd8a
christophe <infobiac1@gmail.com>
2017-11-07 00:00:48 -0800

    flake8

--- tests/test_trading_model.py
+++ tests/test_trading_model.py
@@ -1,23 +1,25 @@
-from unittest import mock, TestCase
 import pytest
 from django.contrib.auth.models import User
-from stocks.models import InvestmentBucket, InvestmentStockConfiguration, Stock
-from trading.models import TradeBucket
+from stocks.models import InvestmentBucket, Stock
 
-@pytest.mark.django_db(transaction=True)
-def test_trading_account_total_value():
-    user = User.objects.create(username='christophe', password="iscool")
-    trading_account = user1.profile.trading_accounts.create(
-        account_name="spesh"
-    )
-
-""" Have to write an available cash test, after fixing 
+# @pytest.mark.django_db(transaction=True)
+# def test_trading_account_total_value():
+#     user = User.objects.create(username='christophe', password="iscool")
+#     trading_account = user.profile.trading_accounts.create(
+#         account_name="spesh"
+#     )
+"""Total value is currently not defined"""
+""" Have to write an available cash test, after fixing
 available cash (current implementation is incorrect) """
+
 
 @pytest.mark.django_db(transaction=True)
 def test_trading_account_available_buckets():
+	"""
+	Test available buckets
+	"""
     user = User.objects.create(username='christophe', password="iscool")
-    trading_account = user1.profile.trading_accounts.create(
+    trading_account = user.profile.trading_accounts.create(
         account_name="spesh"
     )
     buff = InvestmentBucket(name="buffet", owner=user.profile, public=False, available=1).save()
@@ -27,15 +29,18 @@ def test_trading_account_available_buckets():
     trading_account.trade_bucket(buff, 2342342342342234)
     assert trading_account.available_buckets(buff) == 2342342342342235
     trading_account.trade_bucket(buff, -2342342342342234)
-    assert trading_accounts.available_buckets(buff) == 1
+    assert trading_account.available_buckets(buff) == 1
     trading_account.trade_bucket(buff, -1)
-    assert trading_accounts.available_buckets(buff) == 0
+    assert trading_account.available_buckets(buff) == 0
 
 
 @pytest.mark.django_db(transaction=True)
 def test_trading_account_available_stock():
+	"""
+	Test available stocks
+	"""
     user = User.objects.create(username='christophe', password="iscool")
-    trading_account = user1.profile.trading_accounts.create(
+    trading_account = user.profile.trading_accounts.create(
         account_name="spesh"
     )
     stock = Stock(name="sto", ticker="sto").save()
@@ -45,68 +50,98 @@ def test_trading_account_available_stock():
     trading_account.trade_stock(stock, 2342342342342234)
     assert trading_account.available_stock(stock) == 2342342342342235
     trading_account.trade_stock(stock, -2342342342342234)
-    assert trading_accounts.available_stock(stock) == 1
+    assert trading_account.available_stock(stock) == 1
     trading_account.trade_stock(stock, -1)
-    assert trading_accounts.available_stock(stock) == 0
+    assert trading_account.available_stock(stock) == 0
 
-""" Have to write a has enough cash test, after fixing 
+
+""" Have to write a has enough cash test, after fixing
 available cash (current implementation is incorrect) """
+
 
 @pytest.mark.django_db(transaction=True)
 def test_has_enough_bucket():
-	user = User.objects.create(username='christophe', password="iscool")
-    trading_account = user1.profile.trading_accounts.create(
+	"""
+	Test has enough bucket
+	"""
+    user = User.objects.create(username='christophe', password="iscool")
+    trading_account = user.profile.trading_accounts.create(
         account_name="spesh"
-    )
+        )
     buff = InvestmentBucket(name="buffet", owner=user.profile, public=False, available=1).save()
-    assert trading_account.has_enough_bucket(buff, 1) == False
+    assert trading_account.has_enough_bucket(buff, 1) is False
     trading_account.trade_bucket(buff, 1)
     assert trading_account.has_enough_bucket(buff, 1)
-    assert trading_account.has_enough_bucket(buff, 2) == False
+    assert trading_account.has_enough_bucket(buff, 2) is False
     trading_account.trade_bucket(buff, 2342342342342234)
     assert trading_account.has_enough_buckets(buff, 2342342342342235)
-    assert trading_account.has_enough_bucket(buff, 2342342342342236) == False
+    assert trading_account.has_enough_bucket(buff, 2342342342342236) is False
     trading_account.trade_bucket(buff, -2342342342342234)
     assert trading_account.has_enough_bucket(buff, 1)
-    assert trading_account.has_enough_bucket(buff, 2) == False
+    assert trading_account.has_enough_bucket(buff, 2) is False
     trading_account.trade_bucket(buff, -1)
-    assert trading_account.has_enough_bucket(buff, 1) == False
+    assert trading_account.has_enough_bucket(buff, 1) is False
 
 
 @pytest.mark.django_db(transaction=True)
 def test_has_enough_stock():
-	user = User.objects.create(username='christophe', password="iscool")
-    trading_account = user1.profile.trading_accounts.create(
+	"""
+	Test has enough stock
+	"""
+    user = User.objects.create(username='christophe', password="iscool")
+    trading_account = user.profile.trading_accounts.create(
         account_name="spesh"
     )
     stock = Stock(name="sto", ticker="sto").save()
-    assert trading_account.has_enough_stock(stock, 1) == False
+    assert trading_account.has_enough_stock(stock, 1) is False
     trading_account.trade_stock(stock, 1)
     assert trading_account.has_enough_stock(stock, 1)
-    assert trading_account.has_enough_stock(stock, 2) == False
+    assert trading_account.has_enough_stock(stock, 2) is False
     trading_account.trade_stock(stock, 2342342342342234)
     assert trading_account.enough_stock(stock, 2342342342342235)
-    assert trading_account.has_enough_stock(stock, 2342342342342236) == False
+    assert trading_account.has_enough_stock(stock, 2342342342342236) is False
     trading_account.trade_stock(stock, -2342342342342234)
     assert trading_account.has_enough_stock(stock, 1)
-    assert trading_account.has_enough_stock(stock, 2) == False
+    assert trading_account.has_enough_stock(stock, 2) is False
     trading_account.trade_stock(stock, -1)
-    assert trading_account.has_enough_stock(stock, 1) == False
+    assert trading_account.has_enough_stock(stock, 1) is False
 
 
 @pytest.mark.django_db(transaction=True)
 def test_trading_account_trade_bucket():
+	"""
+	Test trade bucket
+	"""
     user = User.objects.create(username='christophe', password="iscool")
-    trading_account = user1.profile.trading_accounts.create(
+    trading_account = user.profile.trading_accounts.create(
         account_name="spesh"
     )
     buffa = InvestmentBucket(name="buffeta", owner=user.profile, public=False, available=1).save()
     with pytest.raises(Exception):
-    	trading_account.trade_bucket(buffa, -2)
+        trading_account.trade_bucket(buffa, -2)
     trading_account.trade_bucket(buffa, 2)
     with pytest.raises(Exception):
-    	trading_account.trade_bucket(buffa, -3)
-    assert trading_account.has_enough_stock(stock, 2)
+        trading_account.trade_bucket(buffa, -3)
+    assert trading_account.has_enough_bucket(buffa, 2)
     trading_account.trade_bucket(buffa, -2)
-    assert trading_account.available_stock(stock) == 0
+    assert trading_account.available_bucket(buffa) == 0
 
+
+@pytest.mark.django_db(transaction=True)
+def test_trading_account_trade_stock():
+	"""
+	Test trade stock
+	"""
+    user = User.objects.create(username='christophe', password="iscool")
+    trading_account = user.profile.trading_accounts.create(
+        account_name="spesh"
+    )
+    stock = Stock(name="sto", ticker='sto').save()
+    with pytest.raises(Exception):
+        trading_account.trade_stock(stock, -2)
+    trading_account.trade_stock(stock, 2)
+    with pytest.raises(Exception):
+        trading_account.trade_stock(stock, -3)
+    assert trading_account.has_enough_stock(stock, 2)
+    trading_account.trade_stock(stock, -2)
+    assert trading_account.available_stock(stock) == 0
