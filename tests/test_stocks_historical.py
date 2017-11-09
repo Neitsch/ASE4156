001dba8e75575ebfe8184cf2224135c984eea4ad
Nigel Schuster <nigel.schusters@googlemail.com>
2017-11-09 09:17:20 -0500

    Add caching for plaid queries - improves queries by a million times

--- tests/test_stocks_historical.py
+++ tests/test_stocks_historical.py
@@ -5,7 +5,6 @@ from stocks.models import Stock, DailyStockQuote
 from stocks import stock_helper
 import pandas as pd
 from yahoo_historical import Fetcher
-from authentication.plaid_middleware import PlaidMiddleware
 import pytest
 import arrow
 
