2a27d4b358c2f7be8fcc9424c255a48360273da8
christophe <infobiac1@gmail.com>
2017-11-09 20:11:21 -0500

    pylint ignores doc

--- doc/conf.py
+++ doc/conf.py
@@ -34,9 +34,9 @@ django.setup()
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.coverage',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+              'sphinx.ext.coverage',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -170,6 +170,3 @@ texinfo_documents = [
      author, 'ASE4156MIWF', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
