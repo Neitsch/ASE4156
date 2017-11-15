7da728baa2d0fb25e7c2fe71dcaa1061a0c27d17
Nigel Schuster <nigel.schusters@googlemail.com>
2017-11-15 13:12:34 -0500

    Fix lint

--- web/js/components/PersonalStatus/__tests__/PersonalStatus-test.jsx
+++ web/js/components/PersonalStatus/__tests__/PersonalStatus-test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import PersonalStatus from '../PersonalStatus.jsx';
+import PersonalStatus from '../PersonalStatus';
 
 describe('PersonalStatus', () => {
   it('exists', () => {
