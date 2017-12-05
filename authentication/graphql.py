abe4db32c3326e52d438c73e65e3f7927abc8b32
Nigel Schuster <nigel.schusters@googlemail.com>
2017-12-05 13:10:42 -0500

    More documentation

--- authentication/graphql.py
+++ authentication/graphql.py
@@ -15,12 +15,14 @@ from .models import Profile, UserBank
 # pylint: disable=too-few-public-methods
 class GUser(DjangoObjectType):
     """
-    GraphQL representation of a User
+    This is the GraphQL representation of a User object. This should *only* be
+    accessible for the user himself. It wraps the Django User class.
     """
+
     class Meta(object):
         """
-        Meta Model for User. We must make sure to not expose
-        the whole usere object
+        Meta Model for User. Most important to note is the fields we expose:
+        ID, Profile, Username and UserBank
         """
         model = User
         only_fields = ('id', 'profile', 'username', 'userbank')
@@ -29,18 +31,22 @@ class GUser(DjangoObjectType):
 
 class GProfile(DjangoObjectType):
     """
-    GraphQL representation of a Profile
+    This is the GraphQL representation of a User Profile. This is more of a
+    publically accessible object. Even though we won't expose everything, this
+    object allows us to add more fields to the user object.
     """
     stock_find = List(
-        NonNull(GStock), args={'text': Argument(NonNull(String)), 'first': Argument(Int)})
-    invest_suggestions = DjangoConnectionField(
-        GInvestmentBucket,
-    )
+        NonNull(GStock),
+        args={'text': Argument(NonNull(String)),
+              'first': Argument(Int)})
+    invest_suggestions = DjangoConnectionField(GInvestmentBucket, )
     selected_acc = NonNull(GTradingAccount)
 
     class Meta(object):
         """
-        Meta Model for Profile
+        Meta Model for Profile. We have edges to the Trading Accounts the User
+        has, a field that is the current Trading Account a User has, and a
+        function that allows the user to search for stocks.
         """
         model = Profile
         only_fields = ('id', 'trading_accounts', 'selected_acc', 'stock_find')
@@ -50,6 +56,12 @@ class GProfile(DjangoObjectType):
     def resolve_stock_find(_self, _info, text, first=None, **_args):
         """
         Finds a stock given a case insensitive name
+
+        :param text: The text the user want to search for.
+        :type name: str.
+        :param first: The maximum number of results to return
+        :type name: int.
+        :returns:  QuerySet.
         """
         if first:
             return Stock.find_stock(text, first)
@@ -58,27 +70,37 @@ class GProfile(DjangoObjectType):
     @staticmethod
     def resolve_invest_suggestions(_data, info, **_args):
         """
-        Finds all the investment suggestions available to the user
+        Returns a list of buckets that the User can invest in.
+
+        :param info: Information about the user to check which recommendations
+            are best for the user.
+        :type info: Graphene Request Info.
+        :returns:  QuerySet.
         """
         return InvestmentBucket.accessible_buckets(info.context.user.profile)
 
     @staticmethod
     def resolve_selected_acc(data, _info, **_args):
         """
-        Returns the selected account. For now we just assume the user has only 1
+        Returns the current account the user has selected. Right now it just
+        calls the default account of the profile.
+
+        :returns:  QuerySet.
         """
         return data.default_acc()
 
 
 class GUserBank(DjangoObjectType):
     """
-    GraphQL representation of a UserBank
+    GraphQL wrapper around the UserBank model. This should *only* be accessible
+    to the user
     """
     balance = NonNull(Float)
     income = NonNull(Float)
     name = NonNull(String)
     outcome = NonNull(Float)
-    history = NonNull(List(NonNull(GDataPoint)), args={'start': Argument(NonNull(String))})
+    history = NonNull(
+        List(NonNull(GDataPoint)), args={'start': Argument(NonNull(String))})
 
     class Meta(object):
         """
@@ -95,8 +117,7 @@ class GUserBank(DjangoObjectType):
         """
         return [
             DataPoint(date, value)
-            for (date, value)
-            in data.historical_data(start)
+            for (date, value) in data.historical_data(start)
         ]
 
     @staticmethod
@@ -143,6 +164,8 @@ class Query(object):
         if not info.context.user.is_authenticated():
             return None
         return info.context.user
+
+
 # pylint: enable=no-init
 
 
@@ -150,11 +173,13 @@ class AddTradingAccount(Mutation):
     """
     AddTradingAccount creates a new TradingAccount for the user
     """
+
     class Arguments(object):
         """
         Arguments to create a trading account. Right now it's only a name.
         """
         name = String()
+
     account = Field(lambda: GTradingAccount)
 
     @staticmethod
@@ -163,9 +188,9 @@ class AddTradingAccount(Mutation):
         Creates a TradingAccount and saves it to the DB
         """
         account = TradingAccount(
-            profile=info.context.user.profile,
-            account_name=name
-        )
+            profile=info.context.user.profile, account_name=name)
         account.save()
         return AddTradingAccount(account=account)
+
+
 # pylint: enable=too-few-public-methods
